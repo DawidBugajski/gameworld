3420493074880500ee5e936753f37cb202e7ce08
Dawid <dawidbugajski1996@gmail.com>
2023-02-25 15:45:48 +0100

    implementing redux

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setToggleSideBar } from 'store/slices/sideBarSlice';
 
 const SideBar = () => {
-  return <div className='w-[300px] bg-black text-white h-screen'>SideBar</div>;
+  const dispatch = useDispatch();
+  const { width } = useSelector((state) => state.sideBar);
+  const handleToggle = () => dispatch(setToggleSideBar());
+
+  return (
+    <div className={`w-[${width}px] bg-black text-white h-screen`}>
+      <button onClick={handleToggle}>Toggle</button>
+    </div>
+  );
 };
 
 export default SideBar;
---
+++ src/store/slices/sideBarSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const sideBarSlice = createSlice({
+  name: 'sideBar',
+  initialState: { width: 300 },
+  reducers: {
+    setToggleSideBar: (state) => {
+      if (state.width === 300) {
+        state.width = 100;
+      } else {
+        state.width = 300;
+      }
+    },
+  },
+});
+
+export const { setToggleSideBar } = sideBarSlice.actions;
+export default sideBarSlice.reducer;
