8d23037374a1fe486663146eacd9690b6f9f0643
Dawid <dawidbugajski1996@gmail.com>
2023-03-10 23:00:32 +0100

    linking Carousel.jsx component with GameContainer (API)

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const Carousel = () => {
+const Carousel = ({ data }) => {
   const settings = {
     dots: true,
     infinite: true,
@@ -30,47 +30,11 @@ const Carousel = () => {
       },
     ],
   };
-  return (
-    <div className='overflow-hidden'>
-      {/* <Slider {...settings}>
-        {data.map((game) => (
-          <div key={game.id}>
-            <h3>{game.title}</h3>
-          </div>
-        ))}
-      </Slider> */}
 
-      <h2> Multiple items </h2>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
-      </Slider>
-    </div>
+  console.log(data);
+
+  return (
+    <div className='overflow-hidden'>{<Slider {...settings}></Slider>}</div>
   );
 };
 
--- src/components/GamesContainer.jsx
+++ src/components/GamesContainer.jsx
@@ -16,33 +16,39 @@ const GamesContainer = () => {
           axios.get(`${BASE_URL}?key=${API_KEY}&ordering=-ordering&page=${i}`)
         );
       }
+      // const responses = await Promise.all(promises);
+      // const games = responses.map((res) => res.data.results);
+      // return games.flat();
       const responses = await Promise.all(promises);
-      const games = responses.map((res) => res.data.results);
-      return games.flat();
+      const games = responses.reduce(
+        (acc, curr) => acc.concat(curr.data.results),
+        []
+      );
+      return games;
     },
   });
   if (isLoading) return 'Loading...';
   if (error) return `Error ${error.message}`;
-
-  const top100Games = (
-    <div className='flex flex-wrap h-full  bg-[#181A1B] cursor-grab'>
-      {data.map((game) => (
-        <div
-          key={game.id}
-          className='flex-grow w-20 h-40 m-2 transition-all duration-300 scale-90 bg-center bg-cover rounded-2xl hover:scale-100 hover:brightness-50 lg:w-48 lg:h-72'
-          style={{ backgroundImage: `url(${game.background_image})` }}
-        >
-          <h2 className='text-xl opacity-0'>{game.name}</h2>
-        </div>
-      ))}
-    </div>
-  );
-
+  console.log(data);
   return (
     <div className='bg-black'>
-      <Carousel />
+      <Carousel data={data} />
     </div>
   );
 };
 
 export default GamesContainer;
+
+// const top100Games = (
+//   <div className='flex flex-wrap h-full  bg-[#181A1B] cursor-grab'>
+//     {data.map((game) => (
+//       <div
+//         key={game.id}
+//         className='flex-grow w-20 h-40 m-2 transition-all duration-300 scale-90 bg-center bg-cover rounded-2xl hover:scale-100 hover:brightness-50 lg:w-48 lg:h-72'
+//         style={{ backgroundImage: `url(${game.background_image})` }}
+//       >
+//         <h2 className='text-xl opacity-0'>{game.name}</h2>
+//       </div>
+//     ))}
+//   </div>
+// );
