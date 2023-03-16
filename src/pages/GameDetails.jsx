d55bdd7a82de6361ab8f13497bdf23aac9165a4a
Dawid <dawidbugajski1996@gmail.com>
2023-03-16 21:30:31 +0100

    customising game details page

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { API_KEY, BASE_URL } from 'utils/constans';
+import PageBackgroundImage from 'components/PageBackgroundImage';
+import Platforms from 'components/Platforms';
 
 const GameDetails = () => {
   const { id } = useParams();
@@ -17,38 +19,28 @@ const GameDetails = () => {
   if (error) {
     return <div>An error has occurred: {error.message}</div>;
   }
-
-  const { name, background_image: image } = data;
   console.log(data);
+  const {
+    name,
+    background_image: image,
+    playtime,
+    released,
+    parent_platforms: platforms,
+  } = data;
+
   return (
     <div className='h-screen bg-[#151515] shadow-left relative'>
-      <div className='relative z-10 flex flex-col items-center justify-center mt-20'>
+      <div className='container relative z-10 flex flex-col items-center justify-center mx-auto mt-20'>
+        <Platforms
+          playtime={playtime}
+          released={released}
+          platforms={platforms}
+        />
         <h1 className='text-4xl italic uppercase'>{name}</h1>
       </div>
-      <div className='absolute top-0 left-0 w-full h-full'>
-        <div className='h-[500px]'>
-          <div
-            className='w-full h-full bg-center bg-cover lg:bg-top'
-            style={{
-              backgroundImage: `linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.8), rgba(21, 21, 21, 0.5)), url(${image})`,
-            }}
-          ></div>
-        </div>
-      </div>
+      <PageBackgroundImage image={image} />
     </div>
   );
 };
 
 export default GameDetails;
-
-// co bym chciał tu umieścić, description?. background image, genres, metacritic, parent platforms,
-
-/*
-linear-gradient(rgba(15, 15, 15, 0), rgb(21, 21, 21)), linear-gradient(rgba(21, 21, 21, 0.8), rgba(21, 21, 21, 0.5))
-example: witcher 3
-https://api.rawg.io/api/games/3328/screenshots?key=76cb8b2b755e459baa2b0262c9aadfef - list of screenshots (there not always in id endpoint, so.)
-https://api.rawg.io/api/games/3328/stores?key=76cb8b2b755e459baa2b0262c9aadfef - stores, where you can buy the game
-https://api.rawg.io/api/games/3328/additions?key=76cb8b2b755e459baa2b0262c9aadfef - additions, ex. blood and wine.
-https://api.rawg.io/api/games/3328/game-series?key=76cb8b2b755e459baa2b0262c9aadfef - games related to world lore
-
-*/
