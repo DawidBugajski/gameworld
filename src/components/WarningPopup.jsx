5fca1a8dae2776e4e7b5d5d72b96f76210de799a
Dawid <dawidbugajski1996@gmail.com>
2023-04-06 21:24:00 +0200

    imlement warning popup while clearing wishlist

--- src/components/WarningPopup.jsx
+++ src/components/WarningPopup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {
   setClearWishlist,
@@ -6,15 +6,58 @@ import {
 } from 'store/slices/wishlistSlice';
 
 const WarningPopup = () => {
+  const [isClearing, setIsClearing] = useState(false);
   const dispatch = useDispatch();
-  const handleClearWishlist = () => dispatch(setClearWishlist());
+  const handleClearWishlist = () => {
+    setIsClearing(true);
+    setTimeout(() => {
+      setIsClearing(false);
+      dispatch(setClearWishlist());
+    }, 1500);
+  };
   const handleHideWarningPopup = () => dispatch(setHideClearPopupWarning());
+
   return (
-    <div className='fixed inset-0 z-50 flex items-center justify-center'>
+    <div className='fixed inset-0 z-50 flex items-center justify-center '>
       <div className='absolute inset-0 bg-black opacity-75'></div>
-      <div className='relative z-10 flex justify-around p-8 text-black bg-white'>
-        <button onClick={handleClearWishlist}>YES</button>
-        <button onClick={handleHideWarningPopup}>NO</button>
+      <div className='relative z-10 p-1 rounded-lg bg-gradient-to-br from-main-red via-purple-700 to-main-dark-red'>
+        <div className='flex flex-col gap-6 p-8 font-mono text-lg text-white bg-black'>
+          <p>Are you sure you want to clear your wishlist?</p>
+          <div className='flex justify-center gap-6 transition-all duration-1000'>
+            <button
+              className='transition-colors duration-150 border-transparent px-2 py-1 font-bold tracking-widest text-red-700 uppercase border-[1px] hover:border-red-700 focus:outline-none focus:shadow-outline'
+              onClick={handleHideWarningPopup}
+            >
+              Reject
+            </button>
+            <button
+              className={`transition-colors duration-150 flex items-center px-4 py-1 font-bold tracking-widest text-green-700 uppercase focus:outline-none focus:shadow-outline border-[1px] border-transparent hover:border-green-700 ${
+                isClearing && 'border-green-700'
+              }`}
+              onClick={handleClearWishlist}
+            >
+              {isClearing && (
+                <svg
+                  aria-hidden='true'
+                  className='w-5 h-5 mr-2 text-green-700 animate-spin dark:text-green-700 fill-black'
+                  viewBox='0 0 100 101'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z'
+                    fill='currentColor'
+                  />
+                  <path
+                    d='M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z'
+                    fill='currentFill'
+                  />
+                </svg>
+              )}
+              {isClearing ? 'Clearing' : 'Agree'}
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -17,11 +17,13 @@ const Wishlist = () => {
       <span className='text-[100px] fixed lg:text-[150px] 2xl:top-[10%] left-[4%] top-[15%] rotate-[15deg] pointer-events-none 2xl:text-[150px] text-[#F2F2F2B3] opacity-5 min-[1800px]:text-[200px]'>
         鬼滅の刃 刀鍛冶の里編 ᕦ(⋋‿☄)ᕗ
       </span>
-      <button className='p-2 text-2xl' onClick={handleShowWarningPopup}>
-        Clear wishlist
-      </button>
+      {games.length >= 1 && (
+        <button className='p-2 text-2xl' onClick={handleShowWarningPopup}>
+          Clear wishlist
+        </button>
+      )}
       <div className='relative flex mx-auto'>
-        <span className='pl-3 pr-12 text-6xl italic tracking-widest uppercase pointer-events-none wishlist-title brightness-50'>
+        <span className='pl-3 pr-12 mt-2 text-6xl italic tracking-widest uppercase pointer-events-none wishlist-title brightness-50'>
           Wishlist
         </span>
         <div className='flex flex-wrap gap-6'>
@@ -40,5 +42,4 @@ export default Wishlist;
 // max 100 games or comunicate, buy premium and fuck off
 // show counter how many left games you can add ?
 // filtering games by ?
-// draggable games?
 // you dont know what buy first? let's roll (add spin animate and after 2sec throw popup with losed game)
