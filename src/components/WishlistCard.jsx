2f732c599bc9fa655b25479100404eb53b89ed06
Dawid <dawidbugajski1996@gmail.com>
2023-04-11 22:43:54 +0200

    fix wishlist for mobile

--- src/components/WishlistCard.jsx
+++ src/components/WishlistCard.jsx
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
 import { setRemoveFromWishlist } from 'store/slices/wishlistSlice';
 import { useDispatch } from 'react-redux';
+import useWindowWidth from 'hooks/useWindowWidth';
 
 const WishlistCard = ({ image, name, id }) => {
+  const { isMobile } = useWindowWidth();
   const dispatch = useDispatch();
 
   const handleDeleteGame = (game) => dispatch(setRemoveFromWishlist(game));
-
-  return (
+  const desktopLayout = (
     <div
       key={id}
-      className='outline-none p-2 sm:p-0 w-1/2 h-60 sm:h-60 sm:w-40 md:w-48 relative transition-all duration-150 rounded-lg sm:shadow-xl cursor-pointer min-[1800px]:w-52 wish-card md:h-72 group sm:hover:shadow-cyan-800 sm:hover:shadow-2xl hover:scale-95'
+      className='outline-none p-2 sm:p-0 w-1/2 h-60 sm:h-60 sm:w-40 md:w-48 relative transition-all duration-150 rounded-lg sm:shadow-xl cursor-pointer min-[1800px]:w-52 md:h-72 group sm:hover:shadow-cyan-800 sm:hover:shadow-2xl hover:scale-95'
     >
       <FaTrash
         onClick={() => handleDeleteGame({ image, name, id })}
@@ -34,6 +35,33 @@ const WishlistCard = ({ image, name, id }) => {
       </Link>
     </div>
   );
+  const mobileLayout = (
+    <Link className='inline-block w-1/2 p-2' key={id} to={`/games/${id}`}>
+      <div className='relative rounded-lg outline-none h-60'>
+        <FaTrash
+          onClick={(e) => {
+            e.preventDefault();
+            handleDeleteGame({ image, name, id });
+          }}
+          className='absolute z-[50] w-6 h-6 transition-colors duration-150 top-4 right-4 '
+        />
+        <img
+          alt={name}
+          src={
+            image !== null
+              ? image
+              : 'https://via.placeholder.com/600x400?text=Game+Image'
+          }
+          className='object-cover w-full h-full rounded-xl brightness-50'
+        />
+        <h3 className='border-main-red border-l-2 px-3 py-1 absolute w-full text-sm font-bold uppercase cursor-pointer bottom-[10%] left-1/2-translate-x-1/2'>
+          {name}
+        </h3>
+      </div>
+    </Link>
+  );
+
+  return <>{isMobile ? mobileLayout : desktopLayout}</>;
 };
 
 export default WishlistCard;
