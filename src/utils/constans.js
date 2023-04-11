0f4fc8b520de619cc81b552b1f36eaa0e2ea064d
Dawid <dawidbugajski1996@gmail.com>
2023-04-11 23:54:04 +0200

    new apikey

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -1,6 +1,6 @@
 import { getNextDay } from './helpers/getNextDay';
 
-export const API_KEY = '76cb8b2b755e459baa2b0262c9aadfef';
+export const API_KEY = '7efe092c929749abb1398edae2a86ee5';
 export const BASE_URL = 'https://api.rawg.io/api/games';
 export const PREMIERE_GAMES_URL = `${BASE_URL}?dates=${getNextDay()},2024-12-31&ordering=-rating&key=${API_KEY}&page_size=10`;
 
